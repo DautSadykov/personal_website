e80fb8dc7c76a52a7a51d9494d008885d3464efc
DautSadykov <sadykov.daut@yandex.ru>
2023-03-31 13:43:14 +0500

    developed navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Navbar() {
+    const [isBurgerClicked, setIsBurgerClicked] = useState(false)
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+    
+    function handleToggleNavbar() {
+        setIsBurgerClicked ((prevIsBurgerClicked) => !prevIsBurgerClicked)
+    }
+
+    function hideBurger() {
+        if (windowWidth > 870) {
+            setIsBurgerClicked(false)
+        }
+    }
+
+    function handleResize() {
+        setWindowWidth(window.innerWidth)
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", handleResize)
+        hideBurger()
+        return() => {
+            window.removeEventListener("resize", handleResize)
+        }
+    })
+
+    // console.log(isBurgerClicked)
+
     return(
         <div className="header">
             <div className="logo">
                 <img src="my_logo.png" alt="" />
             </div>
-            <div className="toggle_navbar_button">
+            <div onClick={handleToggleNavbar} className="toggle_navbar_button">
                 <span className="bar"></span>
                 <span className="bar"></span>
                 <span className="bar"></span> 
             </div>
-            <nav className="navbar">
+            <nav className={`navbar ${isBurgerClicked ? 'clicked' : 'unclicked'}`}>
                 <ul>
                     <li className="clickable">HOME</li>
                     <li className="clickable">ABOUT</li>
@@ -19,7 +46,7 @@ export default function Navbar() {
                     <li className="clickable">PORTFOLIO</li>
                     <li className="clickable">BLOG</li>
                     <li className="clickable">CONTACT</li>
-                    <li>|</li>
+                    <li className="separator">|</li>
                     <li className="phone_number">
                         <img className="phone_icon" src="phone_icon.png" alt="" />
                         <div>+7 922 351-90-45</div>
